014aea9d60fc2881dbd1087744900470207417af
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-06 12:18:57 +0530

    2

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,15 +2,15 @@ import { FaTimes, FaEdit } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, deleteFeedback , editFeedback }) {
 
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
-      <button onClick={{}} className='close'>
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
         <FaTimes color='purple' />
       </button>
-      <button onClick={{}} className='edit'>
+      <button onClick={() => editFeedback(item)} className='edit'>
         <FaEdit color='purple' />
       </button>
       <div className='text-display'>{item.text}</div>
