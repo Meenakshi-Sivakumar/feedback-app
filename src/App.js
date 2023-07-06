99c8f1d08ad760f11793fc0829a70aafd2f2816a
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-06 12:18:54 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    setFeedback(feedback.filter((item) => item.id !== id ))
+  }
+  const editFeedback = () => {
+
+  }
   return (
     <div>
     <Header />
-    <FeedbackList />
+    <div className="container">
+    {(!feedback || feedback.length === 0)?
+       (<p>No feedback yet</p>) 
+        : 
+      (
+          <FeedbackList feedback = {feedback} deleteFeedback={deleteFeedback} editFeedback={editFeedback}/>
+      )} 
+    </div>
     </div>
   );
 }
