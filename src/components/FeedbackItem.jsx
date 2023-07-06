b0c7cd98c126fc3ed5cb66ae408321fef2c25baf
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-06 11:56:17 +0530

    4

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,25 @@
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+
+function FeedbackItem({ item }) {
+
+  return (
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={{}} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={{}} className='edit'>
+        <FaEdit color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem
