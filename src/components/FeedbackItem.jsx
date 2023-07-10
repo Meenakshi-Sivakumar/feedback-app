df3523db09771e37d4581817ccf24b1fbec6634c
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-10 14:47:01 +0530

    4

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,11 @@
 import { FaTimes, FaEdit } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem({ item, deleteFeedback , editFeedback }) {
-
+function FeedbackItem({ item}) {
+  const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
