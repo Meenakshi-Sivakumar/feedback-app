03061cbe202f978e9492a91ec26806cb046ef404
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-10 12:02:58 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPages";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -18,19 +21,31 @@ function App() {
   } 
   // todo edit rating 
   return (
-    <div>
-    <Header />
-    <div className="container">
-    <FeedbackForm handleAdd={handleAdd}/>
-    <FeedbackStats feedback={feedback} />
-    {(!feedback || feedback.length === 0)?
-       (<p>No feedback yet</p>) 
-        : 
-      (
-          <FeedbackList feedback = {feedback} deleteFeedback={deleteFeedback} editFeedback={editFeedback}/>
-      )} 
-    </div>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route 
+          exact
+          path="/" 
+          element={
+            <>
+            <div className="container">
+            <FeedbackForm handleAdd={handleAdd}/>
+            <FeedbackStats feedback={feedback} />
+            {(!feedback || feedback.length === 0)?
+              (<p>No feedback yet</p>) 
+                : 
+              (
+                  <FeedbackList feedback = {feedback} deleteFeedback={deleteFeedback} editFeedback={editFeedback}/>
+              )} 
+            </div>
+            </>
+            } 
+          />
+        <Route path="/about" element={<AboutPage />}/>
+      </Routes>
+      <AboutIconLink />
+    </Router>
   );
 }
 
