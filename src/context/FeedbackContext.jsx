e1ffba64fe0d2ed5c56a1b8ff52f267123cb1c3a
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-10 15:14:13 +0530

    3

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -5,20 +5,40 @@ const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
     const [feedback, setFeedback] = useState(FeedbackData)
+    const [feedbackEdit, setFeedbackEdit] = useState({
+      item: {},
+      edit: false
+    })
+
+    //add feedback
     const handleAdd = (newFeedback) => {
         setFeedback([newFeedback, ...feedback])
       }
+
+    //delete feedback
     const deleteFeedback = (id) => {
         setFeedback(feedback.filter((item) => item.id !== id ))
       }
-    const editFeedback = () => {
 
+    //edit feedback
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+          item,
+          edit: true
+        })
     }
+    //update feedback
+    const updateFeedback = (id, updItem) => {
+      setFeedback(feedback.map((item) => (item.id === id? {...item, ...updItem} : item)))
+    }
+
     return <FeedbackContext.Provider value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         handleAdd,
-        editFeedback
+        editFeedback,
+        updateFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
