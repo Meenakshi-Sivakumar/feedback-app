54fc82f5160729351c4e7b51ce0967a1fe944447
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-10 12:59:23 +0530

    4

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-const FeedbackContext createContext()
+const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
     const [feedback, setFeedback] = useState([
