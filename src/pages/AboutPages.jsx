2ece51e67ed0ed0055a2e27d68fa30a2698230d1
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-10 12:02:55 +0530

    2

--- src/pages/AboutPages.jsx
+++ src/pages/AboutPages.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import Card from "../components/shared/Card";
+
 function AboutPage() {
     return (
-        <div>
-        
+        <div className="container">
+            <Card>
+                <div className="about">
+                    <h1>About this project</h1>
+                    <p>This is a react app to leave a feedback for a product or service</p>
+                    <p>Version: 1.0.0</p>
+
+                    <p>
+                        <Link to='/'>Back to 🏡</Link>
+                    </p>
+                </div>
+            </Card>
         </div>
     )
 }
