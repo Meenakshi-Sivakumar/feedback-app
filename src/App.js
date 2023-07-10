0ccb85f1284ee2440322a3338bfad19768eda821
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-10 14:46:45 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from './data/FeedbackData';
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPages";
 import AboutIconLink from "./components/AboutIconLink";
-import { FeedbackProvider } from "./context/FeedbackContext";
+import FeedbackContext, { FeedbackProvider } from "./context/FeedbackContext";
+import { useContext } from "react";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-  const deleteFeedback = (id) => {
-    setFeedback(feedback.filter((item) => item.id !== id ))
-  }
-  const handleAdd = (newFeedback) => {
-    setFeedback([newFeedback, ...feedback])
-  }
-  const editFeedback = () => {
-    
-  } 
-  // todo edit rating 
+  
   return (
     <FeedbackProvider>
       <Router>
@@ -32,14 +21,9 @@ function App() {
             element={
               <>
               <div className="container">
-              <FeedbackForm handleAdd={handleAdd}/>
+              <FeedbackForm />
               <FeedbackStats />
-              {(!feedback || feedback.length === 0)?
-                (<p>No feedback yet</p>) 
-                  : 
-                (
-                    <FeedbackList deleteFeedback={deleteFeedback} editFeedback={editFeedback}/>
-                )} 
+              <FeedbackList /> 
               </div>
               </>
               } 
