1906dbf6a253edd5dea7769afe8d1e3ac8b1081f
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-10 14:47:03 +0530

    5

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,18 +1,24 @@
 import { createContext, useState } from "react";
+import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'this is the item from context',
-            rating: 10,
-        }
-    ])
+    const [feedback, setFeedback] = useState(FeedbackData)
+    const handleAdd = (newFeedback) => {
+        setFeedback([newFeedback, ...feedback])
+      }
+    const deleteFeedback = (id) => {
+        setFeedback(feedback.filter((item) => item.id !== id ))
+      }
+    const editFeedback = () => {
 
+    }
     return <FeedbackContext.Provider value={{
         feedback,
+        deleteFeedback,
+        handleAdd,
+        editFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
